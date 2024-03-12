9b0d125bc9e1bf6f1bc7b098be5ba51757d8ba94
ttinnar <111982172+ttinnar@users.noreply.github.com>
2024-03-12 14:52:24 +0200

    upd

--- src/rnd.tests.mjs
+++ src/rnd.tests.mjs
@@ -1,7 +1,8 @@
+/* eslint-disable max-len */
 const calculateSum = (...numbers) => {
-  console.log('input values: ', numbers);  //-> input values:  [1, 2, 3, 4, 5] (array)
+  console.log('input values: ', numbers); // -> input values:  [1, 2, 3, 4, 5] (array)
   return numbers.reduce((acc, number) => acc + number, 0);
-}
+};
 calculateSum(1, 2, 3, 4, 5); // 15
 
 const originalArray = [1, 2, 3];

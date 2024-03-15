65ab189559646e0ddf14a4ebb86914b3742e39b9
ttinnar <tinde@hotmail.fi>
2024-03-15 14:43:01 +0200

    upd

--- db/create-db-user-example.sql
+++ db/create-db-user-example.sql
@@ -1,3 +1,3 @@
 CREATE USER 'ttinnar'@'localhost' IDENTIFIED BY 'Kissakissa1234';
-GRANT ALL PRIVILEGES ON `HealthDiary`.* TO 'ttinnar'@'localhost';
+GRANT ALL PRIVILEGES ON `healthdiary`.* TO 'ttinnar'@'localhost';
 FLUSH PRIVILEGES;
